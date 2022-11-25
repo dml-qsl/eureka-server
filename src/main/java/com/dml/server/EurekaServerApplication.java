ed4e824c39c356c1501b2991cbb9b0442641c71d
笔芯 <duhuixiang@192.168.2.9>
2022-11-26 00:45:48 +0800

    Initial commit

---
+++ src/main/java/com/dml/server/EurekaServerApplication.java
@@ -0,0 +1,13 @@
+package com.dml.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServerApplication {
+    public static void main(String[] args) {
+        new SpringApplication(EurekaServerApplication.class).run(args);
+    }
+}
